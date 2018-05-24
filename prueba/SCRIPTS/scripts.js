c3620273939010facc98a28bb771579a2f0db894
corchuelillo <san2nds@hotmail.com>
2018-05-24 13:56:48 +0200

    SELECTORES

--- prueba/SCRIPTS/scripts.js
+++ prueba/SCRIPTS/scripts.js
@@ -1,2 +1,6 @@
 let saludo2= 'hola amigo desde el head'
 console.log(saludo2);
+/*Comentarios multilínea
+Se puede dar un salto o varias líneas  */
+
+// Una sola línea de comentario. No permite saltos de línea
